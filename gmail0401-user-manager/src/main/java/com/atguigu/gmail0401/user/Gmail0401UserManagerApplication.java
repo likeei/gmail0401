73f03ed5523bc47faeb65c6fc09cafbf157a1f21
likeei <likeei1214@163.com>
2020-02-11 11:13:00 +0800

    电商401

---
+++ gmail0401-user-manager/src/main/java/com/atguigu/gmail0401/user/Gmail0401UserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmail0401.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmail0401UserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmail0401UserManagerApplication.class, args);
+    }
+
+}
---
+++ gmail0401-user-manager/src/test/java/com/atguigu/gmail0401/user/Gmail0401UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmail0401.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmail0401UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
